330da2977aa62b36f1eb8185f66f093f6b7ba0cc
Heeji Hyun <wlgmlgus@gmail.com>
2023-01-27 22:28:26 -0500

    commit tree da349708d76fa6dd0055059ea351a20f067f203e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+const submitData = (username, email) => {
+    const formData = {
+        name: username,
+        email: email
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(res => res.json())
+        .then(object => document.body.innerHTML = object["id"])
+        .catch(error => document.body.innerHTML = error.message);
+}
